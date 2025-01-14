b68c5aac0d5ba145fd11a92cf56d052302d90b9d
noah4467 <nvsimons@email.neit.edu>
2025-01-14 11:07:06 -0500

    Lab2 Individual

--- Labs/Lab2_Individual.py
+++ Labs/Lab2_Individual.py
@@ -2,19 +2,28 @@ import csv
 
 totalRecords = 0
 
-with open("Text File/filehandling.csv") as csvfile:\
+print(f"{'TYPE'}    {'BRAND'}   {'CPU'} {'RAM'} {'FIRST DISK'}  {'NO HDD'}   {'2ND DISK'}   {'OS'}  {'YR'}")
+
+with open("Text File/filehandling.csv") as csvfile:
     file = csv.reader(csvfile)
 
     for record in file:
-    totalRecords += 1
-    print(record)
+        totalRecords += 1
+        print(record)
 
-    type = record[0]
-    brand = record[1]
-    cpu = record[2]
-    ram = record[3]
-    firstDisk = [4]
-    noHDD = record[5]
-    secondDisk = record[6]
-    os = record[7]
-    yr = record[8]
+        type_ = record[0]
+        brand = record[1]
+        cpu = record[2]
+        ram = record[3]
+        firstDisk = [4]
+        noHDD = record[5]
+        secondDisk = record[6]
+        os = record[7]
+        yr = record[8]
+
+    if (type_ == "D"):
+        type_ = "Desktop"
+    else:
+        type_ = "Laptop"
+    if (noHDD == 2):
+        
