4b6b307e0840d0a0dd46976e154f70108f6510ac
noah4467 <nvsimons@email.neit.edu>
2025-01-27 11:13:58 -0500

    ivuniorwda

--- practice/W4D1_sequentalSearch copy.py
+++ practice/W4D1_sequentalSearch copy.py
@@ -16,14 +16,14 @@ while answer == "y":
        found = -1 #invalid index number, will use to check later to see if a student has been found
        
         #get search item from user
-        search_name = input("Enter the LAST NAME of the student you are searching for: ")
+       search_name = input("Enter the LAST NAME of the student you are searching for: ")
 
     #perform search
     for i in range(0 , len(lName))
     #display results
     if not found: #the list is empty
        print(f"Your search for {search_grade} was *NOT* found!")
-       
+
     #build a way out of the loop
     if search_type == "1" or search_type == "2":
         answer = input("Would you like to search again? (y or n)").lower()
