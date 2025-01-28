10c1c6b3df9abf92d23a9497766293e73de5c265
noah4467 <nvsimons@email.neit.edu>
2025-01-28 09:56:31 -0500

    demo files

---
+++ Labs/W4D1_sequentalSearch.py
@@ -0,0 +1,156 @@
+#W4D1 sequential searfch
+
+#IMPORTS
+import csv
+#FUNCTIONS
+def letter(num):
+    if num >= 90:
+        let = "A"
+    elif num >= 80:
+        let = "B"
+    elif num >= 70:
+        let = "C"
+    elif num >= 60:
+        let = "D"
+    elif num < 60:
+        let = "F"
+    else:
+        let = "ERROR"
+
+    return let #let value replaces the function call in the main executing code.
+
+
+#MAIN EXECUTING CODE
+
+#create empty lists to hold the file data
+fName = []
+lName = []
+test1 = []
+test2 = []
+test3 = []
+
+with open("Text File/class_grades.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        #append the file data into appropriate lists
+        fName.append(rec[0])
+        lName.append(rec[1])
+        test1.append(int(rec[2]))
+        test2.append(int(rec[3]))
+        test3.append(int(rec[4]))
+
+#disconnect from file -- can still access the stored data via the lists
+
+#process the list data to calculate an avg score for each student, and find a letter grade equivalent
+num_avg = [] #will hold each students numeric average of test scores
+let_avg = [] #will hold each students letter average of test scores
+
+for i in range(0, len(fName)):
+
+    #calculate average of current student
+    a = (test1[i] + test2[i] + test3[i]) / 3
+    #add average to num_avg list
+    num_avg.append(a)
+
+    l = letter(a) #return value of letter() stored to l
+    let_avg.append(l) #can also do: let_avg.append(letter(a))
+
+#process the lists to display all student data back to the user
+
+print(f"{'FNAME':10}   {'LNAME':10}   {'T1':3}  {'T2':3}  {'# AVG':6}   {'L AVG'}")
+
+for i in range(0, len(fName)):
+    print(f"{fName[i]:10}   {lName[i]:10}   {test1[i]:3}  {test2[i]:3}  {num_avg[i]:6.2f}   {let_avg[i]}")
+
+print("---------------------------------------------------------------------------------------------------")
+
+print(f"There are {len(fName)} students in the file. ")
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+print("\n\nWelcome to the Student Search Program\n\n")
+answer = input("Would you like to begin searching? (y or n)").lower()
+
+while answer == "y":
+    #get search type from user
+    print("\tSEARCH MENU OPTIONS")
+    print("1. Search by LAST name")
+    print("2. Search by LETTER GRADE")
+    print("3. EXIT")
+
+    search_type = input("Enter your search type [1-3]: ")
+
+    if search_type == "1":
+       print("\tSEARCH BY LAST NAME") 
+
+       found = -1 #invalid index number, will use to check later to see if a student has been found
+       
+        #get search item from user
+       search_last = input("Enter the LAST NAME of the student you are searching for: ")
+
+    #perform search
+       for i in range(0, len(lName)):
+            #for loop performs the SEQUENCE - from start through end of list items
+
+            if search_last.lower() == lName[i].lower(): 
+                #if performs the search
+                found = i  #stores found item's INDEX LOCATION
+
+        #step 3: display results to user
+       if found != -1:
+            #last name FOUND!
+            print(f"Your search for {search_last} was FOUND! Here is their data: ")
+            print(f"{fName[found]:10}  {lName[found]:10}  {test1[found]:3}  {test2[found]:3}  {test3[found]:3}  {num_avg[found]:6.1f}  {let_avg[found]}")
+       else: 
+            #NOT found
+            print(f"Your search for {search_last} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+    
+    elif search_type == "2": #LETTER GRADE
+        print("\tLETTER GRADE SEARCH")
+
+        #sequential search - search for a collection of students based on their Letter Grade Average
+        found = []
+        search_let= input("Enter the LETTER GRADE you wish to find: ")
+
+        #step 2: perform search algo (seq. search -> for loop w/ if statement)
+        for i in range(0, len(let_avg)):
+            #for loop performs the sequence
+
+            if search_let.upper() == let_avg[i]: 
+                #if performs the SEARCH -
+                found.append(i)
+                print(f"Found a {search_let} grade in INDEX {i}")
+
+        #step 3: display results to user; make sure you give info: both for found or NOT found
+        if not found:
+            #NOT found
+            print(f"Your search for {search_let} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+        else: 
+            #last name FOUND
+            print(f"Your search for {search_let} was FOUND! Here is their data: ")
+
+            #found is a list populated with index locations
+            for i in range(0, len(found)):
+                print(f"{found[i]}:  {fName[found[i]]:10}  {lName[found[i]]:10}  {test1[found[i]]:3}  {test2[found[i]]:3}  {test3[found[i]]:3}  {num_avg[found[i]]:6.1f}  {let_avg[found[i]]}")
+    elif search_type == "3": #exit
+        print("\t~EXIT~")
+        answer = "x"
+    else:
+        print("\t!INVALID ENTRY!")
+    
+    #build a way out of the loop - answer should be able to change valu
+    if search_type == "1" or search_type == "2":
+        answer = input("Would you like to search again? [y/n]: ").lower()
+
+
+print("\nThanks for using the search program. Goodbye!\n")
---
+++ practice/W4D2_searchReview.py
@@ -0,0 +1,81 @@
+#W4D2 Sequential Search Revview + Creating & Writing to Text Files
+
+#IMPORTS
+import csv
+#FUNCTIONS
+
+#MAIN EXECUTING CODE
+
+#create empty list one for each field
+dragons = []
+riders = []
+count = []
+color1 = []
+color2 = []
+
+with open("Text File/dragons.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        dragons.append(rec[0])
+        riders.append(rec[1])
+        count.append(rec[2])
+
+        color1.append(rec[3])
+
+        if rec[2] == "2":
+            color2.append(rec[4])
+        elif rec[2] == "1":
+            color2.append("---")
+        else:
+            color2.append("ERROR")
+            
+#disconnected from file
+
+#process the lists to display data to the console
+print(f"{'DRAGONS':15}  {'RIDERS':30}  {'#':3}  {'COLOR1':8}  {'COLOR2'}")
+print("------------------------------------------------------------------------")
+
+for i in range(0, len(dragons)):
+    print(f"{dragons[i]:15}  {riders[i]:30}  {count[i]:3}  {color1[i]:8}  {color2[i]}")
+
+#SEARCH FOR A SPECIFIC DRAGON
+found = "x"
+search = input("Which dragon are you looking for:")
+
+for i in range(0, len(dragons)):
+    if search.lower() == dragons[i].lower():
+        #hold onto the found location of our searched for calue
+        found = i
+#filter and display results
+if found != "x": #search was found!
+    print(f"Your search for {search} was FOUND:")
+    print(f"{dragons[found]:15}  {riders[found]:30}  {count[found]:3}  {color1[found]:8}  {color2[found]}")
+    
+else:
+    print(f"Your search for {search} was NOT found:")
+
+
+#SEARCH FOR COLOR SET
+found = []
+search = input("Enter the dragon color you are looking for: ")
+
+for i in range(0, len(color1)):
+
+    if search.lower() in color1[i] or search.lower() in color2[i]:
+        found.append(i)
+
+if not found: #if the found list is empty*
+    print(f"your search for {search} could not be found")
+else:
+    print(f"your search for {search} was found")
+    for i in range(0, len(found)):
+        print(f"{dragons[found[i]]:15}  {riders[found[i]:30]}   {count[found[i]]:3} {color1[found[i]]:8}    {color2[found[i]]:8}")
+
+#WRITE DATA TO NEW FILE
+
+file = open('Text File/test3.csv', 'w')
+for i in range(0, len(dragons)):
+    file.write(f"{dragons[i]},{riders[i]}\n")
+
+file.close()
