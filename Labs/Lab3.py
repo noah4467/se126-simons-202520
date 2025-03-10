0941f473bfc9fb04ef0ea162a532043fe187eb4e
noah4467 <nvsimons@email.neit.edu>
2025-03-10 09:23:57 -0400

    lab3 redo

--- Labs/Lab3.py
+++ Labs/Lab3.py
@@ -8,6 +8,10 @@
 #totalEntrees: the number of entrees into the program
 #numEligableNot: the number of people who are eligable to vote but did not vote
 #numVoted: the number of people who voted
+#idNumber: the id number attached to each user in the csv file
+#age: the age attached to each user in the csv file
+#ifRegistered: whether the user is eligable to vote. either "Y or N"
+#ifVoted: whether the user voted or not. either "Y or N"
 
 import csv
 
@@ -18,34 +22,39 @@ numVoted = 0
 numEligableNot = 0
 numVoted = 0
 totalEntries = 0
+#defines the lists as empty
+idNumber = []
+age = []
+ifRegistered = []
+ifVoted = []
 
 with open("Text_File/voters_202040.csv") as csvfile:
 
     file = csv.reader(csvfile)
-
+    #appends csv data to lists
     for rec in file:
         totalEntries = totalEntries + 1
-        idNumber = rec[0]
-        age = int(rec[1])
-        ifRegistered = rec[2]
-        ifVoted = rec[3]
+        idNumber.append(rec[0])
+        age.append(int(rec[1]))
+        ifRegistered.append(rec[2])
+        ifVoted.append(rec[3])
 
 #Main process
-        
-        if (age < 18):
-            numUnableReg = numUnableReg + 1
+for i in range(0, len(idNumber)):    
+    if (age[i] < 18):
+        numUnableReg = numUnableReg + 1
+    else:
+        numEligable = True 
+        if (ifRegistered[i] == "N"):
+            numOldEnough = numOldEnough + 1
         else:
-            numEligable = True 
-            if (ifRegistered == "N"):
-                numOldEnough = numOldEnough + 1
+            if (ifVoted[i] == "N"):
+                numEligableNot = numEligableNot + 1
             else:
-                if (ifVoted == "N"):
-                    numEligableNot = numEligableNot + 1
-                else:
-                    numVoted = numVoted + 1
+                numVoted = numVoted + 1
 
 
-#prints the final result of the user-inputted data
+#prints the final number of people in each category
 
 print("The number of people not eligable to register is ", numUnableReg)
 print("The number of individuals who are able to vote but have not registered is ", numOldEnough)
